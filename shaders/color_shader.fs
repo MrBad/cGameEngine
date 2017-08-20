ce42f322e4fd4a8217fb0128c167c7280aa825f8
vio <vio@pad>
2017-08-20 21:26:41 +0300

    shader

--- shaders/color_shader.fs
+++ shaders/color_shader.fs
@@ -16,11 +16,12 @@ void main()
 	vec4 textureColor = texture(mySampler, fragmentUV);
 
 //	color = vec4(0.4, 0.0, 0.0, 1.0);
-	color = vec4(fragmentColor.r * (cos(fragmentPosition.x*4 + time)+ 1.0) * 0.5,
+/*	color = vec4(fragmentColor.r * (cos(fragmentPosition.x*4 + time)+ 1.0) * 0.5,
 				 fragmentColor.g * (cos(fragmentPosition.y*6 + time)+ 1.0) * 0.5,
 				 fragmentColor.b * (cos(fragmentPosition.x*2 * 0.4 + time)+ 1.0) * 0.3,
 	fragmentColor.a) * textureColor;
-
+*/
+	color = vec4(fragmentColor.r, fragmentColor.g, fragmentColor.b, fragmentColor.a) * textureColor;
 
 /*	color = fragmentColor + vec4(
 			1.0 * (cos(time) + 1.0) * 0.5,
